9d0e53a366755a92074ec18a6305e1bb801b6ad1
Evan Haston <ehaston@darkmatterit.io>
2023-08-30 08:24:47 -0400

    feature: adding tensorflow

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 from waitress import serve
 import tensorflow_decision_forests as tfdf
@@ -36,12 +37,17 @@ def predict():
         owns_car_int = int(data['owns_car'])
 
         # Create a 1D NumPy array
-        single_record = np.array(
-            [data['age'], data['number_of_accidents'], data['number_of_tickets'], married_int, owns_car_int,
-             data['years_licensed']])
-        tf_serving_dataset = tfdf.keras.pd_dataframe_to_tf_dataset(single_record, label=None)
+        input_df = pd.DataFrame([{
+            'age': age,
+            'number_of_accidents': number_of_accidents,
+            'number_of_tickets': number_of_tickets,
+            'married': married,
+            'owns_car': owns_car,
+            'years_licensed': years_licensed
+        }])
+        tf_serving_dataset = tfdf.keras.pd_dataframe_to_tf_dataset(input_df, label=None)
 
-        prediction = insurance_model.predict(tf_serving_dataset,
+        prediction = insurance_model.predict(input_df,
                                              verbose=0).tolist()  # Converting numpy array to list for jsonify
 
         return jsonify({'prediction': prediction[0]})
